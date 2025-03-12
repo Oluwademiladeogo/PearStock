c311e720a1b1cfc28789bd5fae8a975cc9566e5f
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2025-03-12 10:44:12 +0100

    chore: rename files for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Sidebar from "./components/SideBar/SideBar";
 import Dashboard from "./components/pages/Dashboard";
 import Login from "./components/pages/login";
-import Items from "./components/pages/Items";
+import Products from "./components/pages/Products";
 import Signup from "./components/pages/Signup";
 import Logout from "./components/pages/Logout";
 import Home from "./components/pages/home";
@@ -28,7 +28,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/items" element={<Items />} />
+          <Route path="/products" element={<Products />} />
           <Route path="/logout" element={<Logout />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/verify-otp" element={<VerifyOtp />} />

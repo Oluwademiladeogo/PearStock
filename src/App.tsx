e4ef7145dc2d22613085d9512b3853eee74d1876
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2025-03-12 09:33:15 +0100

    Feat: implement updated routes in requirements

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Sidebar from "./components/SideBar/SideBar";
 import Dashboard from "./components/pages/Dashboard";
 import Login from "./components/pages/login";
-// import Items from './pages/Items';
-// import Tools from './pages/Tools';
-// import Assets from './pages/Assets';
-// import Project from './pages/Project';
-// import Request from './pages/Request';
-// import OnHand from './pages/OnHand';
-// import GRNReport from './pages/GRNReport';
-// import Logout from './pages/Logout';
+import Items from './components/pages/Items';
+import Signup from "./components/pages/Signup";
+import Logout from "./components/pages/Logout";
+// import Logout from './components/pages/Logout';
 
 function App() {
   const location = useLocation();
@@ -20,16 +16,11 @@ function App() {
       {!hideSidebar && <Sidebar />}
       <div className={`flex-1 p-4 ${hideSidebar ? 'w-full' : ''}`}>
         <Routes>
+          <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/items" element={<Dashboard />} />
-          <Route path="/tools" element={<Dashboard />} />
-          <Route path="/assets" element={<Dashboard />} />
-          <Route path="/project" element={<Dashboard />} />
-          <Route path="/request" element={<Dashboard />} />
-          <Route path="/on-hand" element={<Dashboard />} />
-          <Route path="/grn-report" element={<Dashboard />} />
-          <Route path="/logout" element={<Dashboard />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/logout" element={<Logout />} />
         </Routes>
       </div>
     </div>

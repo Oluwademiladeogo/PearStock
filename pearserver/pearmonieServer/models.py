fed30139738d44adc1dcad3a172c0c6c3529e45c
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2025-03-16 17:22:58 +0100

    db: define custom user and products model

--- pearserver/pearmonieServer/models.py
+++ pearserver/pearmonieServer/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField  
 from django.core.validators import MinValueValidator
 
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, blank=False, null=False)
+    phone_number = PhoneNumberField(blank=True, null=True)
+    street_address = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state_province = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+
+    username = None 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] 
+
+    def __str__(self):
+        return self.email
+    
 class Products(models.Model):
     name = models.CharField(max_length=100, unique=True, blank=False, null=False)
     model = models.CharField(max_length=50, blank=False, null=False)
@@ -13,7 +31,7 @@ class Products(models.Model):
         blank=False, 
         null=False
     )
-    image = models.CharField(max_length=100, blank=False, null=False)
+    image = models.TextField(max_length=100, blank=False, null=False)
     stock = models.PositiveIntegerField(validators=[MinValueValidator(1)], blank=False, null=False)
     
     def __str__(self):

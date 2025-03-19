9b4d0b1e7910a2a4c1c768943cf9519a5d5b3b9d
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2025-03-19 09:20:04 +0100

    feat: implemeent logout logic

--- webclient/pages/logout.tsx
+++ webclient/pages/logout.tsx
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
+
 const Logout = () => {
-  return <div>Logout</div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    // Clear cookies
+    Cookies.remove("token");
+    Cookies.remove("user");
+
+    // Redirect to login page
+    router.push("/login");
+  }, [router]);
+
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      fontSize: '2em', 
+      fontWeight: 'bold',
+      color: '#333' 
+    }}>
+      Logging out...
+    </div>
+  )
 };
 
 export default Logout;

cb1a0777e4622545be5485fca830ea56747466e6
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2025-04-02 15:47:09 +0100

    chore: refactor code

--- pearserver/pearserver/settings.py
+++ pearserver/pearserver/settings.py
@@ -114,6 +114,7 @@ REST_FRAMEWORK = {
 
 #SSL settings for production
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 # Security Settings for Production
 # if not DEBUG:
 #     SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "True").lower() == "true"
